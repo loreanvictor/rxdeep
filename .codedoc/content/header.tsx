ee4e9cef095ebb6dbd08259cba6753967b6d9168
Eugene Ghanizadeh <ghanizadeh.eugene@gmail.com>
2020-07-21 20:42:40 +0200

    Update header.tsx

--- .codedoc/content/header.tsx
+++ .codedoc/content/header.tsx
@@ -6,7 +6,7 @@ export function Header(config: CodedocConfig, renderer: any) {
   return (
     <_Header>{config.misc?.github ?
       <fragment>
-        <a class="github-button" href="https://github.com/sponsors/ntkme" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-heart" aria-label="Sponsor @ntkme on GitHub">Sponsor</a>
+        <a class="github-button" href="https://github.com/sponsors/loreanvictor" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-heart" aria-label="Sponsor @loreanvictor on GitHub">Sponsor</a>
         <br/>
         <GithubButton action={config.misc.github.action || 'Star'}
           repo={config.misc.github.repo}
