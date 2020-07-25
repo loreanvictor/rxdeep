6b178beec934b1f1ebe673fd644ffdb6951c9e8c
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-07-25 11:08:20 +0200

    update rollup configs

--- conf/rollup/es5.js
+++ conf/rollup/es5.js
@@ -1,12 +1,18 @@
-import babel from 'rollup-plugin-babel';
-import { uglify } from "rollup-plugin-uglify";
+import babel from '@rollup/plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 import base from './base';
 
 
 export default Object.assign(base, {
   plugins: [
-    babel({ exclude: 'node_modules/**', presets: ["@babel/preset-env"],}),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ["@babel/preset-env"],
+    }),
     uglify(),
+    nodeResolve(),
   ],
   output: Object.assign(base.output, {
     file: 'dist/bundles/rxdeep.es5.min.js',
--- conf/rollup/es6.js
+++ conf/rollup/es6.js
@@ -1,10 +1,13 @@
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 import base from './base';
 
 
 export default Object.assign(base, {
   plugins: [
     terser(),
+    nodeResolve(),
   ],
   output: Object.assign(base.output, {
     file: 'dist/bundles/rxdeep.es6.min.js',
